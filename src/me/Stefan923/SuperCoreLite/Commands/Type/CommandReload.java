97ee54c2db036662b92180df151462234b30086f
BrowkS <stefanpopescu923@gmail.com>
2019-10-23 18:00:43 +0300

    Now /core reload command sends a message to the command issuer.

--- src/me/Stefan923/SuperCoreLite/Commands/Type/CommandReload.java
+++ src/me/Stefan923/SuperCoreLite/Commands/Type/CommandReload.java
@@ -1,14 +1,14 @@
 package me.Stefan923.SuperCoreLite.Commands.Type;
 
 import me.Stefan923.SuperCoreLite.Commands.AbstractCommand;
-import me.Stefan923.SuperCoreLite.Commands.CommandManager;
 import me.Stefan923.SuperCoreLite.Main;
+import me.Stefan923.SuperCoreLite.Utils.MessageUtils;
 import org.bukkit.command.CommandSender;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class CommandReload extends AbstractCommand {
+public class CommandReload extends AbstractCommand implements MessageUtils {
 
     public CommandReload(AbstractCommand abstractCommand) {
         super(abstractCommand, false, "reload");
@@ -16,7 +16,7 @@ public class CommandReload extends AbstractCommand {
 
     @Override
     protected AbstractCommand.ReturnType runCommand(Main instance, CommandSender sender, String... args) {
-        if (args.length != 1)
+        if (args.length != 2)
             return ReturnType.SYNTAX_ERROR;
 
         Main plugin = Main.instance;
@@ -24,16 +24,19 @@ public class CommandReload extends AbstractCommand {
         if (args[1].equalsIgnoreCase("all")) {
             plugin.getSettingsManager().reload();
             plugin.getLanguageManager().reload();
+            sender.sendMessage(formatAll("&8「&3SuperCore&8」 &fYou have successfully reloaded &ball &fmodules!"));
             return ReturnType.SUCCESS;
         }
 
         if (args[1].equalsIgnoreCase("settings")) {
             plugin.getSettingsManager().reload();
+            sender.sendMessage(formatAll("&8「&3SuperCore&8」 &fYou have successfully reloaded &bsettings &fmodule!"));
             return ReturnType.SUCCESS;
         }
 
         if (args[1].equalsIgnoreCase("languages")) {
             plugin.getLanguageManager().reload();
+            sender.sendMessage(formatAll("&8「&3SuperCore&8」 &fYou have successfully reloaded &blanguages &fmodule!"));
             return ReturnType.SUCCESS;
         }
 
