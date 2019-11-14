b6c4894f9e255ff2d3f42913b2cc552edab1d176
BrowkS <stefanpopescu923@gmail.com>
2019-11-14 20:32:05 +0200

    Added a new command: /whois <player_name> (!) It shows you info about a certain player. (!) Required permission: "supercore.whois"

---
+++ src/me/Stefan923/SuperCoreLite/Commands/Type/CommandWhoIs.java
@@ -0,0 +1,84 @@
+package me.Stefan923.SuperCoreLite.Commands.Type;
+
+import me.Stefan923.SuperCoreLite.Commands.AbstractCommand;
+import me.Stefan923.SuperCoreLite.Language.LanguageManager;
+import me.Stefan923.SuperCoreLite.Main;
+import me.Stefan923.SuperCoreLite.Utils.MessageUtils;
+import me.Stefan923.SuperCoreLite.Utils.User;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class CommandWhoIs extends AbstractCommand implements MessageUtils {
+
+    public CommandWhoIs() {
+        super(true, true, "whois");
+    }
+
+    @Override
+    protected AbstractCommand.ReturnType runCommand(Main instance, CommandSender sender, String... args) {
+        Player senderPlayer = (Player) sender;
+        User user = instance.getUser(senderPlayer);
+
+        FileConfiguration languageConfig = instance.getLanguageManager(user.getLanguage()).getConfig();
+
+        if (args.length != 1)
+            return ReturnType.SYNTAX_ERROR;
+
+        String targetPlayerName = args[0];
+        Player targetPlayer = Bukkit.getPlayer(targetPlayerName);
+
+        if (targetPlayer == null) {
+            senderPlayer.sendMessage(languageConfig.getString("General.Must Be Online"));
+            return ReturnType.FAILURE;
+        }
+
+        String targetPlayerGamemode = "";
+        switch (targetPlayer.getGameMode()) {
+            case ADVENTURE:
+                targetPlayerGamemode = "Adventure";
+                break;
+            case CREATIVE:
+                targetPlayerGamemode = "Creative";
+                break;
+            case SPECTATOR:
+                targetPlayerGamemode = "Spectator";
+                break;
+            case SURVIVAL:
+                targetPlayerGamemode = "Survival";
+                break;
+        }
+
+        senderPlayer.sendMessage(formatAll(languageConfig.getString("Command.WhoIs.Format")
+                .replace("%playername%", targetPlayerName)
+                .replace("%gamemode%", languageConfig.getString("General.Gamemode." + targetPlayerGamemode))
+                .replace("%health%", String.valueOf(targetPlayer.getHealth()))
+                .replace("%hunger%", String.valueOf(targetPlayer.getFoodLevel()))));
+
+        return ReturnType.SUCCESS;
+    }
+
+    @Override
+    protected List<String> onTab(Main instance, CommandSender sender, String... args) {
+        return null;
+    }
+
+    @Override
+    public String getPermissionNode() {
+        return "supercore.whois";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "/whois <player_name>";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Shows info about a certain player.";
+    }
+
+}
