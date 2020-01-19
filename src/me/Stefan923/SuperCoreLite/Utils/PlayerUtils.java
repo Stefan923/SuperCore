010c801f56f8758de3710cdd359ea6a4b5233df1
BrowkS <stefanpopescu923@gmail.com>
2020-01-19 09:40:17 +0200

    Method to convert location to string.

--- src/me/Stefan923/SuperCoreLite/Utils/PlayerUtils.java
+++ src/me/Stefan923/SuperCoreLite/Utils/PlayerUtils.java
@@ -1,6 +1,7 @@
 package me.Stefan923.SuperCoreLite.Utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.Set;
@@ -8,12 +9,24 @@ import java.util.stream.Collectors;
 
 public interface PlayerUtils {
 
+    /* Get a set with all online players that can see someone. */
+
     default Set<Player> onlinePlayers(Player player) {
         return Bukkit.getOnlinePlayers().stream().filter(player::canSee).collect(Collectors.toSet());
     }
 
+    /* Get a set with all online players that can see someone and has certain permission. */
+
     default Set<Player> onlinePlayers(Player player, String permission) {
         return Bukkit.getOnlinePlayers().stream().filter(onlinePlayer -> player.canSee(onlinePlayer) && onlinePlayer.hasPermission(permission)).collect(Collectors.toSet());
+    }
+
+    /* Convets a location object to string. */
+
+    default String locationToString(Location location) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(location.getBlockX()).append(", ").append(location.getBlockY()).append(", ").append(location.getBlockZ()).append(", ").append(location.getWorld());
+        return stringBuffer.toString();
     }
 
 }
