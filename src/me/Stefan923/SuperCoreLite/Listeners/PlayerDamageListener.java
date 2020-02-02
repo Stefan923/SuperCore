381fb421303170dafd6dd323190b72eccd98bb09
BrowkS <stefanpopescu923@gmail.com>
2020-02-02 12:37:52 +0200

    Added new listener for EntityDamageEvent. This listener is used for god mode concept. If god mode is activated, the player will be invincible to any type of damage.

---
+++ src/me/Stefan923/SuperCoreLite/Listeners/PlayerDamageListener.java
@@ -0,0 +1,25 @@
+package me.Stefan923.SuperCoreLite.Listeners;
+
+import me.Stefan923.SuperCoreLite.Main;
+import me.Stefan923.SuperCoreLite.Utils.User;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class PlayerDamageListener implements Listener {
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageEvent event) {
+        Entity damagedEntity = event.getEntity();
+        if (!(damagedEntity instanceof Player))
+            return;
+        Player damagedPlayer = (Player) damagedEntity;
+        User damagedUser = Main.instance.getUser(damagedPlayer);
+
+        if (damagedUser.getGod())
+            event.setCancelled(true);
+    }
+
+}
