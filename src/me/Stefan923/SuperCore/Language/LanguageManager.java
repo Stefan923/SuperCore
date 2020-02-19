235e1b5697b8eefb4df7557a912af6e3bd5a33c8
BrowkS <stefanpopescu923@gmail.com>
2020-02-19 15:28:09 +0200

    Added new translate options for /heal command.

--- src/me/Stefan923/SuperCore/Language/LanguageManager.java
+++ src/me/Stefan923/SuperCore/Language/LanguageManager.java
@@ -54,6 +54,9 @@ public class LanguageManager implements MessageUtils {
         config.addDefault("Command.God.Own God Mode Changed", "&8(&3!&8) &fYour god mode has been %status%&f!");
         config.addDefault("Command.God.Own God Mode Changed By", "&8(&3!&8) &fYour god mode has been %status%&f by &b%sender%&f!");
         config.addDefault("Command.God.Others God Mode Changed", "&8(&3!&8) &b%target%&f's god mode has been %status%&f!");
+        config.addDefault("Command.Heal.Self-Healed", "&8(&3!&8) &fYou have successfully healed yourself&f!");
+        config.addDefault("Command.Heal.Healed By", "&8(&3!&8) &fYou have been healed by &b%sender%&f!");
+        config.addDefault("Command.Heal.Healed Others", "&8(&3!&8) &fYou have successfully healed &b%target%&f!");
         config.addDefault("Command.HelpOp.Format", "&4&k|&cHelpOp&4&k|&r &7%playername%: &f%message%");
         config.addDefault("Command.List.Format", Arrays.asList("&f&m---------&r&b[ &3&m--------------------&r &b]&f&m---------&r", "&8(&3!&8) &fThere are &b%list_size_supercore.list.default% &fplayers online!", "&7 - &fAdmins (&3%list_size_supercore.list.admin%&f): %list_supercore.list.admin%", "&7 - &fDonors (&3%list_size_supercore.list.donor%&f): %list_supercore.list.donor%", "&f&m---------&r&b[ &3&m--------------------&r &b]&f&m---------&r"));
         config.addDefault("Command.List.Name Color", "&b");
