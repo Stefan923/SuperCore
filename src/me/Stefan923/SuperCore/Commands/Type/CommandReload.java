98cb9145848411ee02d2bda94234f6d8a8eee8c2
BrowkS <stefanpopescu923@gmail.com>
2020-04-13 19:36:17 +0300

    Removed symbols that couldn't be shown in console.

--- src/me/Stefan923/SuperCore/Commands/Type/CommandReload.java
+++ src/me/Stefan923/SuperCore/Commands/Type/CommandReload.java
@@ -22,19 +22,19 @@ public class CommandReload extends AbstractCommand implements MessageUtils {
         if (args[1].equalsIgnoreCase("all")) {
             instance.reloadSettingManager();
             instance.reloadLanguageManagers();
-            sender.sendMessage(formatAll("&8「&3SuperCore&8」 &fYou have successfully reloaded &ball &fmodules!"));
+            sender.sendMessage(formatAll("&8[&3SuperCore&8] &fYou have successfully reloaded &ball &fmodules!"));
             return ReturnType.SUCCESS;
         }
 
         if (args[1].equalsIgnoreCase("settings")) {
             instance.reloadSettingManager();
-            sender.sendMessage(formatAll("&8「&3SuperCore&8」 &fYou have successfully reloaded &bsettings &fmodule!"));
+            sender.sendMessage(formatAll("&8[&3SuperCore&8] &fYou have successfully reloaded &bsettings &fmodule!"));
             return ReturnType.SUCCESS;
         }
 
         if (args[1].equalsIgnoreCase("languages")) {
             instance.reloadLanguageManagers();
-            sender.sendMessage(formatAll("&8「&3SuperCore&8」 &fYou have successfully reloaded &blanguages &fmodule!"));
+            sender.sendMessage(formatAll("&8[&3SuperCore&8] &fYou have successfully reloaded &blanguages &fmodule!"));
             return ReturnType.SUCCESS;
         }
 
