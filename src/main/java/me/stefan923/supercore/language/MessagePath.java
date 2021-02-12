264b297ef88ba07236e960494d9b7216710c64df
Stefan923 <stefanpopescu923@gmail.com>
2021-02-12 18:59:29 +0200

    Changed file's path

--- src/main/java/me/stefan923/supercore/language/MessagePath.java
+++ src/main/java/me/stefan923/supercore/language/MessagePath.java
@@ -2,9 +2,11 @@ package me.stefan923.supercore.language;
 
 public enum MessagePath {
 
-    ADMIN_CHAT_MESSAGE_FORMAT("chat.admin.format"),
-    DONOR_CHAT_MESSAGE_FORMAT("chat.donor.format"),
-    PRIVATE_MESSAGE_FORMAT("chat.private-message.format");
+    LANGUAGE_NAME("Language Name"),
+    ADMIN_CHAT_MESSAGE_FORMAT("Chat.Admin.Format"),
+    DONOR_CHAT_MESSAGE_FORMAT("Chat.Donor.Format"),
+    HELP_CHAT_MESSAGE_FORMAT("Chat.HelpOp.Format"),
+    PRIVATE_MESSAGE_FORMAT("Chat.Private Message.Format");
 
     private String messagePath;
 
