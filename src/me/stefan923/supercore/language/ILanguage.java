470b0ca58a8c946a11f699496b281c2ac218d1fc
Stefan923 <stefanpopescu923@gmail.com>
2021-02-12 15:44:18 +0200

    Added ILanguage interface

---
+++ src/me/stefan923/supercore/language/ILanguage.java
@@ -0,0 +1,26 @@
+package me.stefan923.supercore.language;
+
+import java.util.Map;
+
+public interface ILanguage {
+
+    /**
+     * Returns a copy of the language's map of messages.
+     * @return messages - HashMap<MessageType, String>
+     */
+    public Map<MessagePath, String> getMessages();
+
+    /**
+     * Retruns the message corresponding to the requested message path.
+     * @param messagePath - requested message's path
+     * @return requested message - String
+     */
+    public String getMessage(MessagePath messagePath);
+
+    /**
+     * Returns language's name
+     * @return language's name - String
+     */
+    public String getName();
+
+}
