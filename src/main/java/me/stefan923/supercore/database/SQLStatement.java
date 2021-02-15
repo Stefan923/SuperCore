8471a30d66d47dc34cc8c833568a61dee975aee3
Stefan923 <stefanpopescu923@gmail.com>
2021-02-15 11:25:12 +0200

    Fixed typo

--- src/main/java/me/stefan923/supercore/database/SQLStatement.java
+++ src/main/java/me/stefan923/supercore/database/SQLStatement.java
@@ -1,15 +1,15 @@
 package me.stefan923.supercore.database;
 
-public final class SQLStatemet {
+public final class SQLStatement {
 
     public static final String CREATE_USER = "CALL CREATE_USER(?, ?);";
 
     public static final String DELETE_USER_BY_UUID = "CALL DELETE_USER_BY_UUID(?);";
     public static final String DELETE_USER_BY_NAME = "CALL DELETE_USER_BY_NAME(?);";
 
-    public static final String GET_USER_BY_UUID = "SELECT * FROM `{prefix}users` WHERE `uuid` = ?;";
-    public static final String GET_USER_BY_NAME = "SELECT * FROM `{prefix}users` WHERE `username` = ?;";
+    public static final String GET_USER_BY_UUID = "SELECT * FROM `{prefix}view_users` WHERE `uuid` = ?;";
+    public static final String GET_USER_BY_NAME = "SELECT * FROM `{prefix}view_users` WHERE `username` = ?;";
 
-    private SQLStatemet() { }
+    private SQLStatement() { }
 
 }
