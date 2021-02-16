cb2936001850b43edda7a07d443d140baec52da2
Stefan923 <stefanpopescu923@gmail.com>
2021-02-16 12:25:45 +0200

    Added new SQL statements

--- src/main/java/me/stefan923/supercore/database/SQLStatement.java
+++ src/main/java/me/stefan923/supercore/database/SQLStatement.java
@@ -4,11 +4,19 @@ public final class SQLStatement {
 
     public static final String CREATE_USER = "CALL CREATE_USER(?, ?);";
 
+    public static final String ADD_USER_HOME = "CALL ADD_USER_HOME(?, ?, ?, ?, ?, ?, ?);";
+    public static final String ADD_IGNORED_USER = "CALL ADD_IGNORED_USER(?, ?);";
+
     public static final String DELETE_USER_BY_UUID = "CALL DELETE_USER_BY_UUID(?);";
     public static final String DELETE_USER_BY_NAME = "CALL DELETE_USER_BY_NAME(?);";
 
+    public static final String REMOVE_USER_HOME = "CALL REMOVE_USER_HOME(?, ?);";
+    public static final String REMOVE_IGNORED_USER = "CALL REMOVE_IGNORED_USER(?, ?);";
+
     public static final String GET_USER_BY_UUID = "SELECT * FROM `{prefix}view_users` WHERE `uuid` = ?;";
     public static final String GET_USER_BY_NAME = "SELECT * FROM `{prefix}view_users` WHERE `username` = ?;";
+    public static final String GET_USER_HOMES = "SELECT * FROM `{prefix}view_user_homes` WHERE `userUUID` = ?;";
+    public static final String GET_IGNORED_USERS = "SELECT * FROM `{prefix}view_ignored_users` WHERE `userUUID` = ?;";
 
     private SQLStatement() { }
 
