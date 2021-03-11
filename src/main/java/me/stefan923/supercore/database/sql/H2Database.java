ddfafb1a5160e52129dd5ab79bc5aed2c98bd6f7
Stefan923 <stefanpopescu923@gmail.com>
2021-03-11 23:50:23 +0200

    Added SQLDatabase class

--- src/main/java/me/stefan923/supercore/database/sql/H2Database.java
+++ src/main/java/me/stefan923/supercore/database/sql/H2Database.java
@@ -1,164 +1,51 @@
 package me.stefan923.supercore.database.sql;
 
 import me.stefan923.supercore.SuperCore;
-import me.stefan923.supercore.configuration.language.ILanguage;
-import me.stefan923.supercore.configuration.language.LanguageManager;
-import me.stefan923.supercore.configuration.setting.Setting;
-import me.stefan923.supercore.database.IDatabase;
-import me.stefan923.supercore.user.IUser;
-import me.stefan923.supercore.user.User;
 import me.stefan923.supercore.util.LoggerUtil;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.*;
 import java.util.*;
 
-public class H2Database implements IDatabase {
+public class H2Database extends SQLDatabase {
 
-    private static final String MYSQL_SCHEMA_FILENAME = "h2/H2Schema.sql";
-    private static final String MYSQL_VIEWS_FILENAME = "h2/H2Views.sql";
-
-    private static final LanguageManager languageManager = LanguageManager.getInstance();
-
-    private final String url;
-    private final String tablePrefix;
-    private final boolean useUUID;
-
-    private Connection connection;
+    private static final String H2_SCHEMA_FILENAME = "h2/H2Schema.sql";
+    private static final String H2_VIEWS_FILENAME = "h2/H2Views.sql";
 
     public H2Database(String tablePrefix, boolean useUUID) throws SQLException, ClassNotFoundException {
-        String absolutePath = SuperCore.getInstance().getDataFolder().getAbsolutePath();
-        this.url = "jdbc:h2:" + absolutePath + File.separator + "database;mode=MySQL";
-        this.tablePrefix = tablePrefix;
-        this.useUUID = useUUID;
+        super(tablePrefix, "jdbc:h2:" + SuperCore.getInstance().getDataFolder().getAbsolutePath() +
+                File.separator + "database;mode=MySQL", useUUID);
 
         Class.forName("org.h2.Driver");
-
-        connection = getConnection();
     }
 
     @Override
     public void init() {
         try {
-            executeAll(SchemaReader.getStatements(getClass().getClassLoader().getResourceAsStream(MYSQL_SCHEMA_FILENAME)));
-            executeAll(SchemaReader.getStatements(getClass().getClassLoader().getResourceAsStream(MYSQL_VIEWS_FILENAME)));
+            executeAll(SchemaReader.getStatements(getClass().getClassLoader().getResourceAsStream(H2_SCHEMA_FILENAME)));
+            executeAll(SchemaReader.getStatements(getClass().getClassLoader().getResourceAsStream(H2_VIEWS_FILENAME)));
         } catch (IOException e) {
-            LoggerUtil.sendError("H2Database#getIgnoredUsers(String): Couldn't open MySQL database init files.");
+            LoggerUtil.sendSevere("H2Database#getIgnoredUsers(String): Couldn't open MySQL database init files.");
         }
     }
 
     @Override
     public boolean createUser(UUID playerUUID, String playerName) {
-        PreparedStatement preparedStatement = null;
+        if (super.createUser(playerUUID, playerName)) {
+            return createUserData(playerUUID) && createUserSettings(playerUUID);
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void connect() {
         try {
-            preparedStatement = getConnection().prepareStatement(SQLStatement.CREATE_USER.replace("{prefix}", tablePrefix));
-            preparedStatement.setString(1, String.valueOf(playerUUID));
-            preparedStatement.setString(2, String.valueOf(playerName));
-            preparedStatement.execute();
-
-            createUserData(playerUUID);
-            createUserSettings(playerUUID);
+            connection = DriverManager.getConnection(this.url);
         } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#createUser(UUID, String): Couldn't create a new user: User{uuid = " + playerUUID + ", name = " + playerName + "}");
-            return false;
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#createUser(UUID, String): Couldn't close preparedStatement.");
-            }
+            LoggerUtil.sendSevere("H2Database#connect(): Couldn't establish the connection to the database.");
         }
-
-        return true;
-    }
-
-    @Override
-    public boolean deleteUser(IUser user) {
-        Player player = user.getPlayer();
-
-        return useUUID ? deleteUser(player.getUniqueId()) : deleteUser(player.getName());
-    }
-
-    @Override
-    public IUser getUser(Player player) {
-        UUID playerUUID = player.getUniqueId();
-        User user = null;
-
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            preparedStatement = getConnection()
-                    .prepareStatement(SQLStatement.GET_USER_BY_UUID.replace("{prefix}", tablePrefix));
-            preparedStatement.setString(1, String.valueOf(playerUUID));
-
-            resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                String foundUUID = resultSet.getString("uuid");
-                Map<String, Location> homes = getUserHomes(playerUUID);
-                List<String> ignoredUsers = getIgnoredUsers(foundUUID);
-
-                ILanguage language = languageManager.getLanguageByName(resultSet.getString("language"));
-                if (language == null) {
-                    language = languageManager.getLanguageByFileName(Setting.DEFAULT_LANGUAGE);
-                }
-
-                user = new User(player,
-                        language,
-                        resultSet.getString("customNickname"),
-                        resultSet.getBoolean("godMode"),
-                        resultSet.getBoolean("receivingMessages"),
-                        ignoredUsers,
-                        homes);
-            }
-        } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#getuser(Player): Couldn't select this user: User{uuid = " + playerUUID + "}");
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#getuser(Player): Couldn't close preparedStatement or resultSet.");
-            }
-        }
-
-        return user;
-    }
-
-    @Override
-    public Map<String, Location> getUserHomes(UUID playerUUID) {
-        return getUserHomes(String.valueOf(playerUUID));
-    }
-
-    @Override
-    public List<String> getIgnoredUsers(UUID playerUUID) {
-        return getIgnoredUsers(String.valueOf(playerUUID));
-    }
-
-    private void connect() {
-        try {
-            this.connection = DriverManager.getConnection(this.url);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Connection getConnection() throws SQLException {
-        if (this.connection == null || !this.connection.isValid(5)) {
-            this.connect();
-        }
-
-        return this.connection;
     }
 
     private boolean createUserData(UUID playerUUID) {
@@ -168,7 +55,7 @@ public class H2Database implements IDatabase {
             preparedStatement.setString(1, String.valueOf(playerUUID));
             preparedStatement.execute();
         } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#createUserData(UUID): Couldn't create a new user: User{uuid = " + playerUUID + "}");
+            LoggerUtil.sendSevere("H2Database#createUserData(UUID): Couldn't create a new user: User{uuid = " + playerUUID + "}");
             return false;
         } finally {
             try {
@@ -176,7 +63,7 @@ public class H2Database implements IDatabase {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#createUserData(UUID): Couldn't close preparedStatement.");
+                LoggerUtil.sendSevere("H2Database#createUserData(UUID): Couldn't close preparedStatement.");
             }
         }
 
@@ -190,7 +77,7 @@ public class H2Database implements IDatabase {
             preparedStatement.setString(1, String.valueOf(playerUUID));
             preparedStatement.execute();
         } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#createUserSettings(UUID): Couldn't create a new user: User{uuid = " + playerUUID + "}");
+            LoggerUtil.sendSevere("H2Database#createUserSettings(UUID): Couldn't create a new user: User{uuid = " + playerUUID + "}");
             return false;
         } finally {
             try {
@@ -198,141 +85,11 @@ public class H2Database implements IDatabase {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#createUserSettings(UUID): Couldn't close preparedStatement.");
+                LoggerUtil.sendSevere("H2Database#createUserSettings(UUID): Couldn't close preparedStatement.");
             }
         }
 
         return true;
-    }
-
-    private boolean deleteUser(UUID playerUUID) {
-        PreparedStatement preparedStatement = null;
-        try {
-            preparedStatement = getConnection().prepareStatement(SQLStatement.DELETE_USER_BY_UUID.replace("{prefix}", tablePrefix));
-            preparedStatement.setString(1, String.valueOf(playerUUID));
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#deleteUser(UUID): Couldn't delete this user: User{uuid = " + playerUUID + "}");
-            return false;
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#deleteUser(UUID): Couldn't close preparedStatement.");
-            }
-        }
-
-        return true;
-    }
-
-    private boolean deleteUser(String playerName) {
-        PreparedStatement preparedStatement = null;
-        try {
-            preparedStatement = getConnection().prepareStatement(SQLStatement.DELETE_USER_BY_NAME.replace("{prefix}", tablePrefix));
-            preparedStatement.setString(1, playerName);
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#deleteUser(String): Couldn't delete this user: User{name = " + playerName + "}");
-            return false;
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#deleteUser(String): Couldn't close preparedStatement.");
-            }
-        }
-
-        return true;
-    }
-
-    private Map<String, Location> getUserHomes(String playerUUID) {
-        Map<String, Location> homes = new HashMap<>();
-
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            preparedStatement = getConnection()
-                    .prepareStatement(SQLStatement.GET_USER_HOMES.replace("{prefix}", tablePrefix));
-            preparedStatement.setString(1, playerUUID);
-
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                World world = Bukkit.getWorld(resultSet.getString("world"));
-
-                if (world != null) {
-                    homes.put(resultSet.getString("name"),
-                            new Location(world,
-                                    resultSet.getDouble("x"),
-                                    resultSet.getDouble("y"),
-                                    resultSet.getDouble("z"),
-                                    resultSet.getFloat("yaw"),
-                                    resultSet.getFloat("pitch")));
-                }
-            }
-        } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#getUserHomes(String): Couldn't select this user: User{uuid = " + playerUUID + "}");
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#getUserHomes(String): Couldn't close preparedStatement or resultSet.");
-            }
-        }
-
-        return homes;
-    }
-
-    private List<String> getIgnoredUsers(String playerUUID) {
-        List<String> ignoredPlayers = new ArrayList<>();
-
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            preparedStatement = getConnection()
-                    .prepareStatement(SQLStatement.GET_IGNORED_USERS.replace("{prefix}", tablePrefix));
-            preparedStatement.setString(1, playerUUID);
-
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                ignoredPlayers.add(resultSet.getString("ignoredUsername"));
-            }
-        } catch (SQLException e) {
-            LoggerUtil.sendError("H2Database#getIgnoredUsers(String): Couldn't select this user: User{uuid = " + playerUUID + "}");
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#getIgnoredUsers(String): Couldn't close preparedStatement or resultSet.");
-            }
-        }
-
-        return ignoredPlayers;
-    }
-
-    private void executeAll(List<String> queries) {
-        for (String query : queries) {
-            try {
-                PreparedStatement preparedStatement = getConnection().prepareStatement(query.replace("{prefix}", tablePrefix));
-                preparedStatement.execute();
-            } catch (SQLException e) {
-                LoggerUtil.sendError("H2Database#executeAll(List<String>): Couldn't execute the following sql query:\n" + query);
-                e.printStackTrace();
-            }
-        }
     }
     
 }
---
+++ src/main/java/me/stefan923/supercore/database/sql/SQLDatabase.java
@@ -0,0 +1,264 @@
+package me.stefan923.supercore.database.sql;
+
+import me.stefan923.supercore.SuperCore;
+import me.stefan923.supercore.configuration.language.ILanguage;
+import me.stefan923.supercore.configuration.language.LanguageManager;
+import me.stefan923.supercore.configuration.setting.Setting;
+import me.stefan923.supercore.database.IDatabase;
+import me.stefan923.supercore.user.IUser;
+import me.stefan923.supercore.user.User;
+import me.stefan923.supercore.util.LoggerUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import java.sql.*;
+import java.util.*;
+
+public abstract class SQLDatabase implements IDatabase {
+
+    private static final LanguageManager languageManager = SuperCore.getInstance().getLanguageManager();
+
+    protected final String tablePrefix;
+    protected final boolean useUUID;
+    protected final String url;
+
+    protected Connection connection;
+
+    public SQLDatabase(String tablePrefix, String url, boolean useUUID) throws SQLException {
+        this.tablePrefix = tablePrefix;
+        this.url = url;
+        this.useUUID = useUUID;
+        
+        connection = getConnection();
+    }
+
+    @Override
+    public boolean createUser(UUID playerUUID, String playerName) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = getConnection().prepareStatement(SQLStatement.CREATE_USER.replace("{prefix}", tablePrefix));
+            preparedStatement.setString(1, String.valueOf(playerUUID));
+            preparedStatement.setString(2, String.valueOf(playerName));
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            LoggerUtil.sendSevere("SQLDatabase#createUser(UUID, String): Couldn't create a new user: User{uuid = " + playerUUID + ", name = " + playerName + "}");
+            return false;
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LoggerUtil.sendSevere("SQLDatabase#createUser(UUID, String): Couldn't close preparedStatement.");
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean deleteUser(IUser user) {
+        return useUUID ? deleteUser(user.getUUID()) : deleteUser(user.getUserName());
+    }
+
+    @Override
+    public IUser getUser(UUID playerUUID) {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = getConnection()
+                    .prepareStatement(SQLStatement.GET_USER_BY_UUID.replace("{prefix}", tablePrefix));
+            preparedStatement.setString(1, String.valueOf(playerUUID));
+
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String foundUUID = resultSet.getString("uuid");
+                Map<String, Location> homes = getUserHomes(playerUUID);
+                List<String> ignoredUsers = getIgnoredUsers(foundUUID);
+
+                ILanguage language = languageManager.getLanguageByName(resultSet.getString("language"));
+                if (language == null) {
+                    language = languageManager.getLanguageByFileName(Setting.DEFAULT_LANGUAGE);
+                }
+
+                return new User(
+                        UUID.fromString(foundUUID),
+                        resultSet.getString(""),
+                        ignoredUsers,
+                        homes,
+                        resultSet.getString("customNickname"),
+                        language,
+                        resultSet.getBoolean("godMode"),
+                        resultSet.getBoolean("receivingMessages")
+                );
+            }
+        } catch (SQLException e) {
+            LoggerUtil.sendSevere("SQLDatabase#getuser(Player): Couldn't select this user: User{uuid = " + playerUUID + "}");
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LoggerUtil.sendSevere("SQLDatabase#getuser(Player): Couldn't close preparedStatement or resultSet.");
+            }
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Location> getUserHomes(UUID playerUUID) {
+        return getUserHomes(String.valueOf(playerUUID));
+    }
+
+    @Override
+    public List<String> getIgnoredUsers(UUID playerUUID) {
+        return getIgnoredUsers(String.valueOf(playerUUID));
+    }
+
+    protected abstract void connect();
+
+    protected Connection getConnection() throws SQLException {
+        if (this.connection == null || !this.connection.isValid(5)) {
+            this.connect();
+        }
+
+        return this.connection;
+    }
+
+    protected void executeAll(List<String> queries) {
+        for (String query : queries) {
+            try {
+                PreparedStatement preparedStatement = getConnection().prepareStatement(query.replace("{prefix}", tablePrefix));
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                LoggerUtil.sendSevere("SQLDatabase#getIgnoredUsers(String): Couldn't execute the following sql query:\n" + query);
+            }
+        }
+    }
+
+    private boolean deleteUser(UUID playerUUID) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = getConnection().prepareStatement(SQLStatement.DELETE_USER_BY_UUID.replace("{prefix}", tablePrefix));
+            preparedStatement.setString(1, String.valueOf(playerUUID));
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            LoggerUtil.sendSevere("SQLDatabase#deleteUser(UUID): Couldn't delete this user: User{uuid = " + playerUUID + "}");
+            return false;
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LoggerUtil.sendSevere("SQLDatabase#deleteUser(UUID): Couldn't close preparedStatement.");
+            }
+        }
+
+        return true;
+    }
+
+    private boolean deleteUser(String playerName) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = getConnection().prepareStatement(SQLStatement.DELETE_USER_BY_NAME.replace("{prefix}", tablePrefix));
+            preparedStatement.setString(1, playerName);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            LoggerUtil.sendSevere("SQLDatabase#deleteUser(String): Couldn't delete this user: User{name = " + playerName + "}");
+            return false;
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LoggerUtil.sendSevere("SQLDatabase#deleteUser(String): Couldn't close preparedStatement.");
+            }
+        }
+
+        return true;
+    }
+
+    private Map<String, Location> getUserHomes(String playerUUID) {
+        Map<String, Location> homes = new HashMap<>();
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = getConnection()
+                    .prepareStatement(SQLStatement.GET_USER_HOMES.replace("{prefix}", tablePrefix));
+            preparedStatement.setString(1, playerUUID);
+
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                World world = Bukkit.getWorld(resultSet.getString("world"));
+
+                if (world != null) {
+                    homes.put(resultSet.getString("name"),
+                            new Location(world,
+                                    resultSet.getDouble("x"),
+                                    resultSet.getDouble("y"),
+                                    resultSet.getDouble("z"),
+                                    resultSet.getFloat("yaw"),
+                                    resultSet.getFloat("pitch")));
+                }
+            }
+        } catch (SQLException e) {
+            LoggerUtil.sendSevere("SQLDatabase#getUserHomes(String): Couldn't select this user: User{uuid = " + playerUUID + "}");
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LoggerUtil.sendSevere("SQLDatabase#getUserHomes(String): Couldn't close preparedStatement or resultSet.");
+            }
+        }
+
+        return homes;
+    }
+
+    private List<String> getIgnoredUsers(String playerUUID) {
+        List<String> ignoredPlayers = new ArrayList<>();
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = getConnection()
+                    .prepareStatement(SQLStatement.GET_IGNORED_USERS.replace("{prefix}", tablePrefix));
+            preparedStatement.setString(1, playerUUID);
+
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                ignoredPlayers.add(resultSet.getString("ignoredUsername"));
+            }
+        } catch (SQLException e) {
+            LoggerUtil.sendSevere("SQLDatabase#getIgnoredUsers(String): Couldn't select this user: User{uuid = " + playerUUID + "}");
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LoggerUtil.sendSevere("SQLDatabase#getIgnoredUsers(String): Couldn't close preparedStatement or resultSet.");
+            }
+        }
+
+        return ignoredPlayers;
+    }
+
+}
