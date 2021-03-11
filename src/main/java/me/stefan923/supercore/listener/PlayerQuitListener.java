2d61c5126850e0d8dbb0df9d2eccace6a13e563a
Stefan923 <stefanpopescu923@gmail.com>
2021-03-12 00:21:47 +0200

    Added PlayerQuitListener class

---
+++ src/main/java/me/stefan923/supercore/listener/PlayerQuitListener.java
@@ -0,0 +1,21 @@
+package me.stefan923.supercore.listener;
+
+import me.stefan923.supercore.SuperCore;
+import me.stefan923.supercore.user.IUserRepository;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerQuitListener implements Listener {
+
+    private final IUserRepository userRepository = SuperCore.getInstance().getUserRepository();
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        userRepository.remove(userRepository.getUser(player.getUniqueId()));
+    }
+
+}
