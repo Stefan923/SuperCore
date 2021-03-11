136884d5d6f81417419f9a70c439216e1afda2d2
Stefan923 <stefanpopescu923@gmail.com>
2021-03-11 23:52:42 +0200

    Added UserRepository implementation class

--- src/main/java/me/stefan923/supercore/user/UserRepository.java
+++ src/main/java/me/stefan923/supercore/user/UserRepository.java
@@ -1,17 +1,75 @@
 package me.stefan923.supercore.user;
 
+import me.stefan923.supercore.database.IDatabase;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-public interface UserRepository {
+public class UserRepository implements IUserRepository {
 
-    IUser add(User user);
+    private final IDatabase database;
 
-    IUser getUserByName(String name);
+    private final List<IUser> onlineUsers = new ArrayList<>();
 
-    IUser getUserByUuid(String uuid);
+    public UserRepository(IDatabase database) {
+        this.database = database;
+    }
 
-    List<User> getOnlineUsers();
+    @Override
+    public IUser createUser(UUID uuid, String name) {
+        database.createUser(uuid, name);
+        return loadUserByUUID(uuid);
+    }
 
-    IUser delete(User user);
+    @Override
+    public IUser getUserByName(String name) {
+        if (name == null || name.isEmpty()) {
+            return null;
+        }
+
+        IUser user = onlineUsers.stream().filter(onlineUser -> name.equalsIgnoreCase(onlineUser.getUserName())).findFirst().orElse(null);
+        if (user == null) {
+            return loadUserByName(name);
+        }
+
+        return user;
+    }
+
+    @Override
+    public IUser getUserByUUID(UUID uuid) {
+        if (uuid == null) {
+            return null;
+        }
+
+        IUser user = onlineUsers.stream().filter(onlineUser -> uuid.equals(onlineUser.getUUID())).findFirst().orElse(null);
+        if (user == null) {
+            return loadUserByUUID(uuid);
+        }
+
+        return user;
+    }
+
+    @Override
+    public List<IUser> getOnlineUsers() {
+        return new ArrayList<>(onlineUsers);
+    }
+
+    @Override
+    public boolean delete(User user) {
+        return onlineUsers.remove(user);
+    }
+
+    private IUser loadUserByName(String name) {
+        IUser user = database.getUser(name);
+        onlineUsers.add(user);
+        return user;
+    }
+
+    private IUser loadUserByUUID(UUID uuid) {
+        IUser user = database.getUser(uuid);
+        onlineUsers.add(user);
+        return user;
+    }
 
 }
