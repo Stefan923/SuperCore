7b7d00f8888f841ed34db2579ac278ad4a2cb5d0
Stefan923 <stefanpopescu923@gmail.com>
2021-03-12 00:22:59 +0200

    Refactored code

--- src/main/java/me/stefan923/supercore/listener/PlayerQuitListener.java
+++ src/main/java/me/stefan923/supercore/listener/PlayerQuitListener.java
@@ -5,14 +5,14 @@ import me.stefan923.supercore.user.IUserRepository;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuitListener implements Listener {
 
     private final IUserRepository userRepository = SuperCore.getInstance().getUserRepository();
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
 
         userRepository.remove(userRepository.getUser(player.getUniqueId()));
