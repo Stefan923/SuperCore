340b928171667475bfa1de7d57f8d06b4b0c407c
Stefan923 <stefanpopescu923@gmail.com>
2021-03-13 01:06:04 +0200

    Added MessageUtil#centerMessage() method

--- src/main/java/me/stefan923/supercore/util/MessageUtil.java
+++ src/main/java/me/stefan923/supercore/util/MessageUtil.java
@@ -29,4 +29,39 @@ public final class MessageUtil {
         return message;
     }
 
+    public static String centerMessage(String message) {
+        if (message == null || message.isEmpty()) {
+            return null;
+        }
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for (char c : message.toCharArray()) {
+            if (c == '§') {
+                previousCode = true;
+            } else if (previousCode) {
+                previousCode = false;
+                isBold = c == 'l' || c == 'L';
+            } else {
+                FontSize dFI = FontSize.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = 154 - halvedMessageSize;
+        int spaceLength = FontSize.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder spaces = new StringBuilder();
+        while (compensated < toCompensate) {
+            spaces.append(" ");
+            compensated += spaceLength;
+        }
+
+        return formatAll(spaces.toString() + message);
+    }
+
 }
